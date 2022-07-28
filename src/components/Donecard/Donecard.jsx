cf3844097fd2e4354ac4c5311901a0efac3da01e
Kiatbordin Jumratanet <kiatbordin05@gmail.com>
2022-07-29 00:00:18 +0700

    Add all card components

--- src/components/Donecard/Donecard.jsx
+++ src/components/Donecard/Donecard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Ongoingcard.css"
+import "./Donecard.css"
 
 import running from "./icons/running.png";
 import walking from "./icons/walking.png";
@@ -7,25 +7,10 @@ import swimming from "./icons/swimming.png";
 import hiking from "./icons/hiking.png";
 import bicycling from "./icons/bicycling.png";
 import deleteButton from "./icons/deleteButton.png";
-import nextButton from "./icons/nextButton.png"
 
-export function Ongoingcard(props) {
-
+export function Donecard(props) {
+    
     const activityItem = props.activity;
-
-    const confirmDone = () => {
-        let isDone = confirm("Have you finished this activity ?");
-        console.log(isDone);
-
-        // Do backend work and re-render the main page.
-    }
-
-    const confirmGaveup = () => {
-        let isGaveup = confirm("Do you want to give up ?");
-        console.log(isGaveup);
-
-        // Do backend work and refresh the main page.
-    }
 
     const activityIconRender = (type) => {
         switch (type) {
@@ -43,8 +28,8 @@ export function Ongoingcard(props) {
     }
 
     return (
-        <div className="Ongoingcard">
-            <div className="ongoing-card-info">
+        <div className="Donecard">
+            <div className="done-card-info">
 
                 <div className="activity-icon-box">
                     {/* <img src={running} alt="activity-icon" /> */}
@@ -60,12 +45,10 @@ export function Ongoingcard(props) {
                     <span>Description: {activityItem.description}</span>
                 </div>
             </div>
-            <div className="ongoing-card-menu">
-                <button className="done-button" onClick={confirmDone}>Done</button>
-                <button className="gaveup-button" onClick={confirmGaveup}>Gave up</button>
+            <div className="done-card-menu">
+                <span>Rating Star Component</span>
             </div>
             <img className="delete-button" src={deleteButton} alt="delete-icon" />
-            <img className="next-button" src={nextButton} alt="delete-icon" />
         </div>
     );
 }
