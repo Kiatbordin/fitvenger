834b8ba5fc7127a46d3a3521f5fe5ac7c765cafd
Park <nattapol.workwork@gmail.com>
2022-07-29 16:07:42 +0700

    adding ProfileCard component

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,27 @@
+import './ProfileCard.css';
+
+import Sample1 from "./image/Sample1.jpg"
+
+export function ProfileCard({name}) {
+    return (
+        <div className="profileCard">
+            <div className="profile">
+                <div className="profileFirstPart">
+                    <div>
+                        <img src={Sample1} alt="" />
+                    </div>
+                    <div>
+                        <p>{name}</p>
+                    </div>
+                </div>
+                <hr/>
+                <div>
+                    <p>log out</p>
+                </div>
+            </div>
+            <div>
+                <p>BMI : 21.6</p>
+            </div>
+        </div>
+    )
+}
