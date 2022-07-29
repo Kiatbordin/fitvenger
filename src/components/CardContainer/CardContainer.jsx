aac8af537be9d3aa7cb47084ff960841f6ce6a99
Park <nattapol.workwork@gmail.com>
2022-07-29 16:48:39 +0700

    fixing my own issue

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,10 +1,11 @@
+import { ProfileCard } from "../ProfileCard/ProfileCard";
 import "./CardContainer.css";
 
 export function CardContainer() {
     return (
         <div className="cardContainer">
             <div className="card">
-                <h1>title</h1>
+                <ProfileCard name="Supreme Cat"/>
             </div>
             <div className="card">
                 <h1>title</h1>
